e18b1f505cfcb8a6bc51fda6991b6542ac5533a9
Logan McCAnn <loganrmccann@gmail.com>
2021-05-13 11:10:10 -0500

    cleaned code for home and comments for future devs

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,10 +3,10 @@ import { useHistory } from "react-router-dom";
 import API from "../utils/API";
 import UserContext from "../utils/Context";
 import photo from "../images/peak-blogspace-icon.png";
-import LoadingPage from '../components/Loading/index';
+import LoadingPage from "../components/Loading/index";
 import "../stylesheets/login.css";
 
-export default function Login () {
+export default function Login() {
   const history = useHistory();
   const { setUser } = useContext(UserContext);
   const [show, setShow] = useState(false);
@@ -17,8 +17,8 @@ export default function Login () {
   const rememberMeRef = useRef();
 
   useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
+    window.scrollTo(0, 0);
+  }, []);
 
   const showModal = () => {
     setShow(true);
@@ -28,6 +28,28 @@ export default function Login () {
     setShow(false);
   };
 
+// verifying credentials with sql and argon2
+  const checkSqlForUser = async (username, password) => {
+    await API.loginUser(
+      (username = usernameRef.current.value),
+      (password = passwordRef.current.value)
+    ).then(async function (res) {
+      if (res.data === "Invalid Username or Password") {
+        setLoading(false);
+        return alert("Invalid Username or Password, Please Try Again");
+      } else if (res.data === "error retrieving account information") {
+        setLoading(false);
+        return alert("Error retrieving account information, please try again!");
+      } else {
+        document.cookie = res.data.sessToken;
+        setUser(res.data.userData);
+        localStorage.setItem("loggedInUserId", res.data.userData.id);
+        history.push("/home");
+      }
+    });
+  };
+
+// checking input values to validate that all have been filled
   const checkCredentialValidity = (e) => {
     e.preventDefault();
     setLoading(true);
@@ -53,106 +75,86 @@ export default function Login () {
       return checkSqlForUser();
     }
   };
-  const checkSqlForUser = async (username, password) => {
-    await API.loginUser(
-      (username = usernameRef.current.value),
-      (password = passwordRef.current.value)
-    ).then(async function (res) {
-      if (res.data === "Invalid Username or Password") {
-        setLoading(false);
-        return alert("Invalid Username or Password, Please Try Again");
-      }else if(res.data === "error retrieving account information"){
-        setLoading(false);
-        return alert("Error retrieving account information, please try again!")
-      } else {
-        // console.log(res, "LOGAN LOGAN LOGAN")
-        document.cookie = res.data.sessToken;
-        setUser(res.data.userData);
-        localStorage.setItem("loggedInUserId" , res.data.userData.id)
-        history.push("/home");
-      }
-    });
-  };
+
   const navSignup = () => {
     history.push("/signup");
   };
 
-  if(loading === true){
-    return(
-      <div className="load-screen-holder container-fixed">
-      <LoadingPage />
-    </div>
-    )
-  }
-  else{
-  if (show === false) {
+  if (loading === true) {
     return (
-      <div className="login-page container">
-        <div className="div-left"></div>
-        <div className="icon-div">
-          <img src={photo} alt="Peak Icon"></img>
-        </div>
-        <div className="intro-text-cont">
-          <h1>Peak Blog Space</h1>
-          <div className="line"></div>
-          <h4>
-            Lorem ipsum dolor sit amet, conse ctetur adipis cing elit. Pellen
-            tesque pretium enim at nisl luctus effic itur.
-          </h4>
-        </div>
-        <div className="btn-holder">
-          <button className="login-btn">
-            <h2 onClick={showModal}>Login</h2>
-          </button>
-          <button className="signup-btn">
-            <h2 onClick={navSignup}>Sign up</h2>
-          </button>
-        </div>
+      <div className="load-screen-holder container-fixed">
+        <LoadingPage />
       </div>
     );
-  } else if (show === true) {
-    return (
-      <div className="login-page container-fluid">
-        <div className="login-modal">
-          <div className="login-modal-header">
-            <h2>Sign In</h2>
-            <button className="close-modal-btn" onClick={hideModal}>
-              <h1>X</h1>
+  } else {
+    if (!show) {
+      return (
+        <div className="login-page container">
+          <div className="div-left"></div>
+          <div className="icon-div">
+            <img src={photo} alt="Peak Icon"></img>
+          </div>
+          <div className="intro-text-cont">
+            <h1>Peak Blog Space</h1>
+            <div className="line"></div>
+            <h4>
+              Lorem ipsum dolor sit amet, conse ctetur adipis cing elit. Pellen
+              tesque pretium enim at nisl luctus effic itur.
+            </h4>
+          </div>
+          <div className="btn-holder">
+            <button className="login-btn">
+              <h2 onClick={showModal}>Login</h2>
+            </button>
+            <button className="signup-btn">
+              <h2 onClick={navSignup}>Sign up</h2>
             </button>
           </div>
-          <div className="login-modal-body">
-            <div className="input-fields">
-              <input
-                type="text"
-                id="input-text"
-                ref={usernameRef}
-                placeholder="Username(8-20 Characters)"
-              ></input>
-              <input
-                type="password"
-                id="input-text"
-                ref={passwordRef}
-                placeholder="Password(8-20 Characters)"
-              ></input>
+        </div>
+      );
+    } else {
+      return (
+        <div className="login-page container-fluid">
+          <div className="login-modal">
+            <div className="login-modal-header">
+              <h2>Sign In</h2>
+              <button className="close-modal-btn" onClick={hideModal}>
+                <h1>X</h1>
+              </button>
             </div>
-            <label className="login-options-cont">
-              <div>
-                <h4>Remember me</h4>
-                <input type="checkbox" ref={rememberMeRef}></input>
+            <div className="login-modal-body">
+              <div className="input-fields">
+                <input
+                  type="text"
+                  id="input-text"
+                  ref={usernameRef}
+                  placeholder="Username(8-20 Characters)"
+                ></input>
+                <input
+                  type="password"
+                  id="input-text"
+                  ref={passwordRef}
+                  placeholder="Password(8-20 Characters)"
+                ></input>
               </div>
-              <button className="forgot-pass-btn">
-                <p className="forgotPassBtn">Forgot Password?</p>
-              </button>
-            </label>
-            <div className="login-modal-btn">
-              <button onClick={checkCredentialValidity}>
-                <h3>Login</h3>
-              </button>
+              <label className="login-options-cont">
+                <div>
+                  <h4>Remember me</h4>
+                  <input type="checkbox" ref={rememberMeRef}></input>
+                </div>
+                <button className="forgot-pass-btn">
+                  <p className="forgotPassBtn">Forgot Password?</p>
+                </button>
+              </label>
+              <div className="login-modal-btn">
+                <button onClick={checkCredentialValidity}>
+                  <h3>Login</h3>
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
-};
