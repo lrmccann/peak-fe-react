946971829256799cfd929b2b9e07a676803d3ba8
Logan McCAnn <loganrmccann@gmail.com>
2021-06-17 00:44:25 -0500

    working on style for trending blog comp

--- src/components/TrendingBlogs/index.js
+++ src/components/TrendingBlogs/index.js
@@ -9,6 +9,10 @@ export default function TrendingBlogs() {
   const [postThree, setPostThree] = useState();
   const [postFour, setPostFour] = useState();
   const [postFive, setPostFive] = useState();
+  const [postSix, setPostSix] = useState();
+  const [postSeven, setPostSeven] = useState();
+  const [postEight, setPostEight] = useState();
+  const [postNine, setPostNine] = useState();
 
   const mapPosts = (arrOfPosts) => {
     console.log(arrOfPosts);
@@ -17,6 +21,10 @@ export default function TrendingBlogs() {
     setPostThree(arrOfPosts[2]);
     setPostFour(arrOfPosts[3]);
     setPostFive(arrOfPosts[4]);
+    setPostSix(arrOfPosts[5]);
+    setPostSeven(arrOfPosts[6]);
+    setPostEight(arrOfPosts[7]);
+    setPostNine(arrOfPosts[8]);
     setLoading(false);
   };
 
@@ -42,32 +50,47 @@ export default function TrendingBlogs() {
     );
   } else {
     return (
-      <div className="trendingCont">
-        <div className="post-cont-one">
+      <div className="trend-cont container-fixed">
+        <div className="container-left">
           <div className="trend-post-one">
-          <h1>{postOne.post_title}</h1>
-                <img src={postOne.blog_img}></img>
+            <h1>{postOne.post_title}</h1>
+            <img src={postOne.blog_img} alt="Blog Item"></img>
           </div>
           <div className="trend-post-two">
-          <h1>{postTwo.post_title}</h1>
-          <img src={postTwo.blog_img}></img>
-          {/* <h2>{postTwo.}</h2> */}
-          </div>
-        </div>
-        <div className="post-cont-two container-fixed">
-          <div className="trend-cont-three container-fixed">
-          <img src={postThree.blog_img}></img>
-            <h1>{postThree.post_title}</h1>
-          </div>
-          <div className="trend-cont-four container-fixed">
-          <img src={postFour.blog_img}></img>
-            <h1>{postFour.post_title}</h1>
-          </div>
-          <div className="trend-cont-five container-fixed">
-          <img src={postFive.blog_img}></img>
             <h1>{postTwo.post_title}</h1>
+            <img src={postTwo.blog_img} alt="Blog Item"></img>
+          </div>
+          <div className="trend-post-three">
+            <h1>{postThree.post_title}</h1>
+            <img src={postThree.blog_img} alt="Blog Item"></img>
           </div>
         </div>
+        <div className="container-right">
+          <button className="trend-post-four container-fixed">
+          <h1>{postFour.post_title}</h1>
+            <img src={postFour.blog_img} alt="Blog Item"></img>
+          </button>
+          <button className="trend-post-five container-fixed">
+          <h1>{postFive.post_title}</h1>
+            <img src={postFive.blog_img} alt="Blog Item"></img>
+            </button>
+            <button className="trend-post-six container-fixed">
+            <h1>{postSix.post_title}</h1>
+            <img src={postSix.blog_img} alt="Blog Item"></img>
+          </button>
+          <button className="trend-post-seven container-fixed">
+          <h1>{postSeven.post_title}</h1>
+            <img src={postSeven.blog_img} alt="Blog Item"></img>
+          </button>
+          <button className="trend-post-eight container-fixed">
+          <h1>{postEight.post_title}</h1>
+            <img src={postEight.blog_img} alt="Blog Item"></img>
+          </button>
+          <button className="trend-post-nine container-fixed">
+          <h1>{postNine.post_title}</h1>
+            <img src={postNine.blog_img} alt="Blog Item"></img>
+          </button>
+      </div>
       </div>
     );
   }
